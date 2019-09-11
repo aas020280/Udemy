aaad347ad2bc9d371b60df975a6b83c2aa5ca3b9
aas020280 <aas020280@gmail.com>
2019-09-11 16:15:38 -0700

    Added TestNG Dir

---
+++ TestNGTutorial/src/Test/Day2.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Day2 {
+	
+	@Test
+	
+	public void Test_3()
+	{
+		System.out.println("Good");
+	}
+
+}
---
+++ TestNGTutorial/src/Test/Day4.java
@@ -0,0 +1,31 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+	// TODO Auto-generated method stub
+	
+	@Test
+	
+	public void WebLoginHomeLoan()
+	{
+		//selenium
+		System.out.println("WebLoginHomeLoan");
+	}
+	
+	@Test
+	
+	public void MobileLoginHomeLoan() 
+	{
+		//Appium
+		System.out.println("MobileLoginHomeLoan");
+	}
+
+	@Test
+	
+	public void LoginAPIHomeLoan()
+	{
+		//Resr API automation
+		System.out.println("LoginAPIHomeLoan");
+	}
+}
